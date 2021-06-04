a34e016d0b6b6c3af6f5ceb7f2f2aad8103ff1a2
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2021-06-04 17:48:58 +0700

    update font

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,6 +37,7 @@
         <link rel="stylesheet" type="text/css" href="{{asset('assets/website/css/style.css')}}"> <!-- This stylesheet dynamically changed from style.less -->
         <!-- responsive css -->
         <link rel="stylesheet" type="text/css" href="{{asset('assets/website/css/responsive.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('assets/website/css/extend.css')}}">
         <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -106,19 +107,19 @@
                                             <nav class="rs-menu pr-90 md-pr-0">
                                                <ul id="onepage-menu" class="nav-menu">
                                                    <li class="current-menu-item">
-                                                       <a href="#rs-header">Home</a>
+                                                       <a href="#rs-header">หน้าแรก</a>
                                                    </li>
                                                    <li>
-                                                       <a href="#rs-about">About</a>
+                                                       <a href="#rs-about">บริการ</a>
                                                    </li>
                                                    <li>
-                                                       <a href="#rs-services">Services</a>
+                                                       <a href="#rs-services">ลูกค้าของเรา</a>
                                                    </li>
                                                    <li>
-                                                       <a href="#rs-blog">Blog</a>
+                                                       <a href="#rs-blog">บทความ</a>
                                                    </li>
                                                    <li>
-                                                       <a href="#rs-contact">Contact</a>
+                                                       <a href="#rs-contact">ติดต่อ</a>
                                                    </li>
                                                </ul> <!-- //.nav-menu -->
                                             </nav>                                         
